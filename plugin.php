bfc232ea8b445f1002346d106cbfedaec9a4462d
Aria Stewart <aredridel@nbtsc.org>
2011-11-12 14:42:59 -0500

    Products plugin works in a basic fashion

--- plugin.php
+++ plugin.php
@@ -8,20 +8,80 @@ class ProductsLite extends RWPlugin {
     }
 
     public function do_editor_head() {
+        ?>
+<script>
+    jQuery(document).ready(function($) {
+        if(!pagedata.plugins.productsLite) pagedata.plugins.productsLite = {};
+        if(!pagedata.plugins.productsLite.products) pagedata.plugins.productsLite.products = [];
+
+        var productTemplate = {
+            name: "Product Name Here",
+            id: "productID",
+            price: "$0.00",
+            options: [
+                {
+                    name: "Option Name",
+                    price: "$0.00"
+                }
+            ]
+        }
+
+        var ta = $('textarea[name=productsLite]')
+
+        var validate = function validate() {
+            try {
+                pagedata.plugins.productsLite = JSON.parse(this.value)
+                $('#status').text('Valid')
+                $('input,button').attr('disabled', null);
+            } catch(e) {
+                $('#status').text('invalid')
+                $('input,button').attr('disabled', 'disabled');
+            }
+        }
+
+        var update = function update(data) {
+            ta.attr('disabled', null)
+            ta.val(JSON.stringify(data, undefined, 2))
+            validate.apply(ta.get(0))
+            pagedata.plugins.productsLite = data
+        }
+
+        ta.change(validate)
+        ta.keyup(validate)
+
+        update(pagedata.plugins.productsLite)
+
+        $('#addproduct').click(function(ev) {
+            ev.preventDefault();
+            pagedata.plugins.productsLite.products.push(productTemplate)
+            update(pagedata.plugins.productsLite)
+        })
+    })
+</script>
+<style>
+textarea[name=productsLite] { width: 100%; height: 20em; }
+</style>
+        <?php
     }
 
     public function getPageTypeName() {
         return 'Products';
     }
 
-    public function the_editor_content() {
-        echo '<textarea name="productsLite"></textarea>';
+    public function the_editor_content($view) {
+        echo "<button id='addproduct'>Add Product</button>";
+        echo "<div id='status'>Valid</div>";
+        echo "<textarea name='productsLite'>Loading, please wait</textarea>";
     }
 
     public function do_head() {
     }
 
-    public function the_content() {
+    public function the_content($page) {
+        echo "<h1>Products Go here</h1>";
+        echo "<pre>";
+        print_r($page->plugins->productsLite);
+        echo "</pre>";
     }
 }
 
